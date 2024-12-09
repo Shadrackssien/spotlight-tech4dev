4e8fdf26c8944721d42f0e40b88e64335acde76c
Shadrack Essien <shadrack6960@gmail.com>
2024-12-09 07:32:50 +0000

    animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./js/*js"],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ""),
+    files: ["./*.{html,js}"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("taos/plugin")],
+  safelist: [
+    "!duration-[0ms]",
+    "!delay-[0ms]",
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ],
 };
